230e2d5db768a2265171ea31b1268339ad08dbd6
E41 <tsvirkunov.egor41@mail.ru>
2026-03-01 01:07:21 +0300

    Добавил поиск дубликатов и поиск пропусков

--- Очистка данных.py
+++ Очистка данных.py
@@ -1,1 +1,11 @@
-print('hello world')
+import pandas as pd
+import numpy as np
+
+df = pd.read_csv("ultimate_student_productivity_dataset_5000.csv")
+#%% Работа с дубликатами
+print(f" До удаления дубликатов. Rows: {df.shape[0]}, Columns: {df.shape[1]}")
+
+df = df.drop_duplicates()
+print(f" После удаления дубликатов. Rows: {df.shape[0]}, Columns: {df.shape[1]}")
+#%% Работа с пропусками
+print(df.isnull().sum().sum())
